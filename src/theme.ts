3135ddd82035c8bdb6e3cac236ea364bb576affd
Mohammed Lazhari <molazhari@sqli.com>
2019-02-09 20:43:07 +0100

    Update the theme primary color

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,10 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import deepOrang from '@material-ui/core/colors/deepOrange';
 
 const theme = createMuiTheme({
     palette: {
-        type: 'dark'
+        type: 'dark',
+        primary: deepOrang
     },
     typography: {
         useNextVariants: true
