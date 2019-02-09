867b355fa26851f3894d708da4d750ad78c631b4
Mohammed Lazhari <molazhari@sqli.com>
2019-02-09 22:02:44 +0100

    Joining all parts together

--- src/components/ResponseCard.tsx
+++ src/components/ResponseCard.tsx
@@ -7,13 +7,47 @@ import {
 } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ReactJson, { ReactJsonViewProps } from 'react-json-view';
+import toJsonSchema from 'to-json-schema';
 
-type Props = WithStyles<typeof styles>;
+type OwnProps = {
+    data: any;
+};
 
-const ResponseCard: React.FunctionComponent<Props> = ({ classes }) => {
+type Props = OwnProps & WithStyles<typeof styles>;
+
+const jsonViewerProps: ReactJsonViewProps = {
+    src: {},
+    style: {
+        background: 'transparent'
+    },
+    collapseStringsAfterLength: 20,
+    indentWidth: 2,
+    collapsed: 2,
+    theme: 'colors',
+    iconStyle: 'square'
+};
+
+const ResponseCard: React.FunctionComponent<Props> = ({ classes, data }) => {
     return (
         <Card className={classes.root}>
-            <CardContent>Response Data</CardContent>
+            <CardContent>
+                <Grid container spacing={40}>
+                    <Grid item md={6}>
+                        <Typography variant="h6">Response</Typography>
+                        <ReactJson {...jsonViewerProps} src={data} />
+                    </Grid>
+                    <Grid item md={6}>
+                        <Typography variant="h6">JSON Schema</Typography>
+                        <ReactJson
+                            {...jsonViewerProps}
+                            src={toJsonSchema(data)}
+                        />
+                    </Grid>
+                </Grid>
+            </CardContent>
         </Card>
     );
 };
